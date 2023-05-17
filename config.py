abe7c0d33fd53591d3470b5a27b83a7733cbd803
thiagoc.machado <thiagocmachado@yahoo.com.br>
2023-05-17 22:15:37 +0200

    backtest

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ SMA_PERIODS = [3, 5, 7]            # Períodos das médias móveis simples (ajus
 # Parâmetros de aprendizado por reforço
 LEARNING_RATE = 0.01               # Taxa de aprendizado do algoritmo de aprendizado por reforço
 DISCOUNT_FACTOR = 0.99             # Fator de desconto para o algoritmo de aprendizado por reforço
-EXPLORATION_RATE = 0.1             # Taxa de exploração para o algoritmo de aprendizado por reforço
+EXPLORATION_RATE = 0.3             # Taxa de exploração para o algoritmo de aprendizado por reforço (0.1)
 REWARD_MULTIPLIER = 100            # Multiplicador de recompensa por trade (ajusta a escala das recompensas)
 TRADE_PENALTY = 0.001              # Penalidade por manter posição aberta (incentiva trades rápidos)
 TIME_STEPS = 500000                # Número de passos de tempo para o treinamento
@@ -36,8 +36,8 @@ BINANCE_API_KEY = "YOUR_BINANCE_API_KEY"        # Chave da API da Binance
 BINANCE_SECRET_KEY = "YOUR_BINANCE_SECRET_KEY"  # Chave secreta da API da Binance
 
 #Parametros de trading
-TAKE_PROFIT = 5.00                  # Take profit (ajustado para scalping)
-STOP_LOSS = 5.00                    # Stop loss (ajustado para scalping)
+TAKE_PROFIT = 3.00                  # Take profit (ajustado para scalping)
+STOP_LOSS = 3.00                    # Stop loss (ajustado para scalping)
 TRAILING_STOP_LOSS = 1.00           # Stop loss de trailing (ajustado para scalping)
 LEVERAGE = 3                        # Leverage (ajustado para scalping)
 DCA = True                          # DCA (ajustado para scalping)
